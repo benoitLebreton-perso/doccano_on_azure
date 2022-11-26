5405130b6b0e9678ad7e3ce4ffe1350aafe52f58
Benoit Lebreton <benoit.lebreton2112@gmail.com>
2022-11-26 15:48:00 +0100

    FEAT unit testing

--- src/retro_panai/create_project.py
+++ src/retro_panai/create_project.py
@@ -4,58 +4,56 @@ from doccano_client.models.data_upload import Task
 from src.retro_panai.prez_panai_repository import load_prez_panai
 
 
-URL = os.environ['URL']
-USER = os.environ['ADMIN_USERNAME']
-PASSWORD = os.environ['ADMIN_PASSWORD']
-doccano_client = DoccanoClient(base_url=URL)
-doccano_client.login(username=USER, password=PASSWORD)
-PROJECT_NAME = 'Retro panai'
+def main():
+    URL = os.environ['URL']
+    USER = os.environ['ADMIN_USERNAME']
+    PASSWORD = os.environ['ADMIN_PASSWORD']
+    doccano_client = DoccanoClient(base_url=URL)
+    doccano_client.login(username=USER, password=PASSWORD)
+    PROJECT_NAME = 'Retro panai'
 
-doccano_client.user_details.get_current_user_details()
-doccano_client.get_profile()
+    # doccano_client.user_details.get_current_user_details()
+    # doccano_client.get_profile()
 
-project = doccano_client.create_project(
-    name=PROJECT_NAME,
-    description=PROJECT_NAME,
-    project_type="ImageClassification",
-    guideline="identifie les prez de panai",
-    collaborative_annotation=False
-)
+    project = doccano_client.create_project(
+        name=PROJECT_NAME,
+        description=PROJECT_NAME,
+        project_type="ImageClassification",
+        guideline="identifie les prez de panai",
+        collaborative_annotation=False
+    )
 
-prez_panai = load_prez_panai()
-for _, prez in prez_panai.iterrows():
-    prez_filepath = prez["filepath"]
-    image = doccano_client.upload(
-        project_id=project.id, 
-        file_paths=[prez_filepath], 
-        task=Task.IMAGE_CLASSIFICATION,
-        format="ImageFile")
+    prez_panai = load_prez_panai()
+    for _, prez in prez_panai.iterrows():
+        prez_filepath = prez["filepath"]
+        image = doccano_client.upload(
+            project_id=project.id, 
+            file_paths=[prez_filepath], 
+            task=Task.IMAGE_CLASSIFICATION,
+            format="ImageFile")
 
-# example_gen = doccano_client.list_examples(project_id=project.id)
-# images_id = [e.id for e in example_gen]
+    # example_gen = doccano_client.list_examples(project_id=project.id)
+    # images_id = [e.id for e in example_gen]
 
+    for _, prez in prez_panai.iterrows():
+        prez_name = prez["name"]
+        print(prez_name)
+        label_type = doccano_client.create_label_type(
+            project_id=project.id,
+            type="category",
+            text=prez_name,
+            )
+        
+    # doccano_client.list_examples(project_id=5)
+    # doccano_client.member.list(project_id=project.id)
+    # doccano_client.get_members_progress(project.id)
 
+    users = doccano_client.search_users()
 
-for _, prez in prez_panai.iterrows():
-    prez_name = prez["name"]
-    print(prez_name)
-    doccano_client.create_label_type(
-        project_id=project.id,
-        type="category",
-        text=prez_name,
-        )
-    
-# doccano_client.list_examples(project_id=5)
+    members = [doccano_client.add_member(project_id=project.id, username=user.username, role_name="annotator") for user in users]
 
+    # doccano_client.list_roles()
+    return project, image, label_type, users, members
 
-
-
-doccano_client.member.list(project_id=project.id)
-doccano_client.get_members_progress(project.id)
-
-users = doccano_client.search_users()
-
-for user in users:
-    doccano_client.add_member(project_id=project.id, username=user.username, role_name="annotator")
-
-# doccano_client.list_roles()
+if __name__ == '__main__':
+    main()
---
+++ tests/src/retro_panai/test_create_project.py
@@ -0,0 +1,45 @@
+import pytest
+from src.retro_panai.create_project import main
+from tests.mock_factory import *
+
+
+def test_create_project(
+        response_mock, 
+        mock_doccano_auth, 
+        mock_doccano_create_project, 
+        mock_doccano_upload,
+        mock_doccano_fp_process,
+        mock_doccano_task_status,
+        mock_doccano_existing_category_types,
+        mock_doccano_new_category_types,
+        mock_doccano_search_users,
+        mock_doccano_search_fakeuser,
+        mock_doccano_search_fakeuser2,
+        mock_doccano_roles,
+        mock_doccano_add_member):
+    project_id = 1
+    response_mock_ = response_mock(
+        [
+            f'POST {mock_url}/v1/auth/login/ -> 200 :{mock_doccano_auth}',
+            f'POST {mock_url}/v1/projects -> 201 :{mock_doccano_create_project}',
+            f'POST {mock_url}/v1/fp/process/ -> 201 :{mock_doccano_fp_process}',
+            f'GET {mock_url}/v1/tasks/status/1 -> 200 :{mock_doccano_task_status}',
+            f'POST {mock_url}/v1/projects/{project_id}/upload -> 201 : {mock_doccano_upload}',
+            f'GET {mock_url}/v1/projects/{project_id}/category-types -> 200 : {mock_doccano_existing_category_types}',
+            f'POST {mock_url}/v1/projects/{project_id}/category-types -> 200 : {mock_doccano_new_category_types}',
+            f'GET {mock_url}/v1/users?q= -> 200 : {mock_doccano_search_users}',
+            f'GET {mock_url}/v1/users?q=fakeuser -> 200 : {mock_doccano_search_fakeuser}',
+            f'GET {mock_url}/v1/users?q=fakeuser2 -> 200 : {mock_doccano_search_fakeuser2}',
+            f'GET {mock_url}/v1/roles -> 200 : {mock_doccano_roles}',
+            f'POST {mock_url}/v1/projects/{project_id}/members -> 200 : {mock_doccano_add_member}'
+        ])
+    with response_mock_:
+        project, image, label_type, users, members = main()
+        assert project.name == "Fake name"
+        assert project.project_type == ProjectType.IMAGE_CLASSIFICATION
+        assert image.ready == True
+        assert label_type.text == "new fake category"
+        assert len(users) == 2
+        assert users[0].username == 'fakeuser'
+        assert users[1].username == 'fakeuser2'
+        assert members[0].user == users[0].id
